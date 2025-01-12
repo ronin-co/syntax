2daa93b74980674404f4c122a05647d46dff3ffd
Leo Lamprecht <leo@ronin.co>
2025-01-12 22:02:11 +0100

    Ensure correct batch context for queries (#23)

--- tests/queries.test.ts
+++ tests/queries.test.ts
@@ -208,6 +208,51 @@ describe('syntax proxy', () => {
     ]);
   });
 
+  test('using nested function as argument in batch', async () => {
+    // It's important to define the `callback` here, in order to guarantee that the
+    // queries are executed standalone if no batch context is detected.
+    const callback = () => undefined;
+
+    const addProxy = getSyntaxProxy({ rootProperty: 'add', callback });
+    const getProxy = getSyntaxProxy({ rootProperty: 'get', callback });
+    const alterProxy = getSyntaxProxy({ rootProperty: 'alter', callback });
+
+    const queryList = getBatchProxy(() => [
+      addProxy.newUsers.to(() => getProxy.oldUsers()),
+      // Assert whether function chaining is still possible after a nested function call,
+      // since the latter is able to manipulate the batch context.
+      alterProxy
+        .model('newUsers')
+        .to({ slug: 'accounts' }),
+    ]);
+
+    expect(queryList).toEqual([
+      {
+        structure: {
+          add: {
+            newUsers: {
+              to: {
+                __RONIN_QUERY: {
+                  get: { oldUsers: {} },
+                },
+              },
+            },
+          },
+        },
+      },
+      {
+        structure: {
+          alter: {
+            model: 'newUsers',
+            to: {
+              slug: 'accounts',
+            },
+          },
+        },
+      },
+    ]);
+  });
+
   test('using schema query types', async () => {
     const callback = () => undefined;
 
